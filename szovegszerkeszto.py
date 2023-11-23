ef65b9fe25f40b582cc61d5a7b4a4b61dbf8412d
hajdupatya <hajdupatrik0826@gmail.com>
2023-11-23 09:07:16 +0100

    Alap

--- szovegszerkeszto.py
+++ szovegszerkeszto.py
@@ -1,6 +1,22 @@
 from tkinter import *
-aa
-dsohgéoíshghíségéodísgísgéouhís
+from tkinter import filedialog
 root=Tk()
-root.title("Szövegszerkesztikt")
+root.title("Szövegszerkesztő")
+root.geometry("600x400")
+
+
+Cim=Label(root,text="Mikroszoft Vord",font=("Centaur",20))
+Cim.pack(pady=10 )
+
+nagyentry= Text(root, width=70, height=12)
+nagyentry.pack(pady=10 )
+
+kisentry=Entry(root, width=50)
+kisentry.pack(pady=10 )
+
+betoltes=Button(root, text="Betöltés")
+betoltes.pack(pady=10 )
+
+mentes=Button(root, text="Mentés")
+mentes.pack()
 root.mainloop()
