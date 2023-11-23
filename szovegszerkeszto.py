08bc19a3228464ddb93017ba08eb02810f3f5177
kobi1414 <kobanbarna86@gmail.com>
2023-11-23 10:11:18 +0100

    színek

--- szovegszerkeszto.py
+++ szovegszerkeszto.py
@@ -24,25 +24,25 @@ def uj():
     szoveg.delete("1.0", END)
     szoveg.configure(state=DISABLED)
 
-Cim=Label(root,text="Mikroszoft Vord",font=("Centaur",20))
+Cim=Label(root,text="Mikroszoft Vord",font=("Centaur",20),bg="#003060",fg="white")
 Cim.pack(pady=10 )
 
-szoveg= Text(root, width=62, height=9, state=DISABLED ,font=("Centaur",14))
+szoveg= Text(root, width=62, height=9, state=DISABLED ,font=("Centaur",14),bg="#0E86D4",fg="white")
 szoveg.pack(pady=10 )
 
-kisentry=Entry(root, width=40,font=("Centaur",14))
+kisentry=Entry(root, width=40,font=("Centaur",14),bg="#0E86D4",fg="white")
 kisentry.pack(pady=10 )
 
-gombframe=LabelFrame(root,border=0)
+gombframe=LabelFrame(root,border=0,bg="#003060")
 gombframe.pack(pady=10)
 
-betoltes=Button(gombframe, text="Betöltés", command=betolt,font=("Centaur",15))
+betoltes=Button(gombframe, text="Betöltés", command=betolt,font=("Centaur",15),bg="#055C9D",fg="white")
 betoltes.grid(row=0, column=0 ,padx=10)
 
-mentes=Button(gombframe, text="Mentés", command=ment,font=("Centaur",15))
+mentes=Button(gombframe, text="Mentés", command=ment,font=("Centaur",15),bg="#055C9D",fg="white")
 mentes.grid(row=0, column=1 ,padx=10)
 
-new=Button(gombframe,text="Új fájl", command=uj,font=("Centaur",15))
+new=Button(gombframe,text="Új fájl", command=uj,font=("Centaur",15),bg="#055C9D",fg="white")
 new.grid(row=0, column=2 ,padx=10)
-
+root.configure(bg="#003060")
 root.mainloop()
